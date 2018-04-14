8e58b11f38fdb1fb5b160f7ce69da0a280078372
Gawayne Lawton <gawayne.lawton@mymona.uwi.edu>
2018-04-14 05:33:27 +0000

    Built form using Flask-WTF

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired
+from flask_wtf.file import FileAllowed, FileRequired, FileField 
+from flask import Flask, render_template, flash, session, redirect, url_for
+from wtforms import TextAreaField, SelectField
+
+
+class UploadForm(FlaskForm):
+    description = TextAreaField("Description", validators=[InputRequired()])
+    photo = FileField("Photo", validators=[FileRequired(), FileAllowed(['jpg','png','jpeg'], 'Only image files accepted.')])
+
 
